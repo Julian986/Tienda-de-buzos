4a2f8e4d7dfea607c949e9c7c3ab9b2627f3381d
Julisan <julisan2911@gmail.com>
2024-09-02 19:56:05 -0300

    arreglando cositad del indek

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var imagen = document.querySelectorAll('img');
+var imagen = document.getElementById('imagen1');
 
 function nuevaPestania(){
-    window.open('detail.html');
+  //  window.open('detail.html'); // Se abrira en una nueva pestaña
+     window.location.href = 'detail.html'; // Se abre en la misma pestaña
 }
 
-imagen.addEventListenner('click', nuevaPestania());
+imagen.addEventListener("click", nuevaPestania);
